3b4035336fdc819077226ba5f4378bc0f7aff1dc
Zach <strongz@umich.edu>
2017-11-18 15:58:35 -0500

    Set up caching

--- FinalProject.py
+++ FinalProject.py
@@ -23,6 +23,30 @@ import sqlite3
 ## Caching Setup:
 ##		- 100 interactions needed (posts, emails, commits, likes, etc.)
 
+CACHE_FNAME = "206_APIsAndDBs_cache.json"
+# Put the rest of your caching setup here:
+try:
+	cache_file = open(CACHE_FNAME, 'r')
+	cache_contents = cache_file.read()
+	CACHE_DICTION = json.loads(cache_contents)
+	cache_file.close()
+except:
+	CACHE_DICTION = {}
+
+# Define your function get_user_tweets here:
+def get_user_tweets(user):
+	if user in CACHE_DICTION:
+		print("Data was in the cache")
+		return CACHE_DICTION[user]
+	else:
+		print("Making a request for new data...")
+		results = api.user_timeline(user)
+		CACHE_DICTION[user] = results
+		f = open(CACHE_FNAME, "w")
+		f.write(json.dumps(CACHE_DICTION))
+		f.close()
+		return CACHE_DICTION[user]
+
 ## Function to Find Days Interactions Took Place (Sunday - Monday):
 
 ## Write Data to Database:
