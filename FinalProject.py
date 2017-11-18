151f3666d7934588402af7717a4fce2479f05233
Zach <strongz@umich.edu>
2017-11-17 22:14:57 -0500

    Final Project Specs

---
+++ FinalProject.py
@@ -0,0 +1,31 @@
+## SI 206 2017
+## Final Project
+
+## Zachary Strong
+
+
+## IMPORTS:
+
+
+## APIs to Be Used:
+##		- Facebook
+##		- GitHub
+##		- Instagram
+##		- Gmail
+##		- YouTube
+
+## Visualization Tools to Be Used:
+##		- Pandas
+##		- Google Maps
+
+
+## Caching Setup:
+##		- 100 interactions needed (posts, emails, commits, likes, etc.)
+
+## Function to Find Days Interactions Took Place (Sunday - Monday):
+
+## Write Data to Database:
+
+## REPORT:
+##		- Screen display, file output, etc.
+##		- Shows how active you are on each day on the site
