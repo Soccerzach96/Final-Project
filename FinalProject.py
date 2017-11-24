f4040f90e51b09021a2868da2da19b27d3e806e7
Zach <strongz@umich.edu>
2017-11-24 18:27:38 -0500

    Started setting up Coinbase API

--- FinalProject.py
+++ FinalProject.py
@@ -8,49 +8,10 @@
 import json
 import sqlite3
 
-## APIs to Be Used:
-##		- Facebook
-##		- GitHub
-##		- Instagram
-##		- Gmail
-##		- YouTube
-
-## Visualization Tools to Be Used:
-##		- Pandas
-##		- Google Maps
+import coinbase_info
+from coinbase.wallet.client import client
 
 
-## Caching Setup:
-##		- 100 interactions needed (posts, emails, commits, likes, etc.)
 
-CACHE_FNAME = "206_APIsAndDBs_cache.json"
-# Put the rest of your caching setup here:
-try:
-	cache_file = open(CACHE_FNAME, 'r')
-	cache_contents = cache_file.read()
-	CACHE_DICTION = json.loads(cache_contents)
-	cache_file.close()
-except:
-	CACHE_DICTION = {}
 
-# Define your function get_user_tweets here:
-def get_user_tweets(user):
-	if user in CACHE_DICTION:
-		print("Data was in the cache")
-		return CACHE_DICTION[user]
-	else:
-		print("Making a request for new data...")
-		results = api.user_timeline(user)
-		CACHE_DICTION[user] = results
-		f = open(CACHE_FNAME, "w")
-		f.write(json.dumps(CACHE_DICTION))
-		f.close()
-		return CACHE_DICTION[user]
-
-## Function to Find Days Interactions Took Place (Sunday - Monday):
-
-## Write Data to Database:
-
-## REPORT:
-##		- Screen display, file output, etc.
-##		- Shows how active you are on each day on the site
+client = Client(coinbase_info.api_key, coinbase_info.api_secret)
