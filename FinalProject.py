3a39ab0f869202f57d333d21eeca761f66ddc763
Zach <strongz@umich.edu>
2017-12-01 23:01:02 -0500

    Connected to Coinbase API

--- FinalProject.py
+++ FinalProject.py
@@ -10,9 +10,12 @@ import sqlite3
 
 ## Connect to Coinbase API
 import coinbase_info
-from coinbase.wallet.client import client
-
+from coinbase.wallet.client import Client
 
 
 ## Create Client Connection to Coinbase API
 client = Client(coinbase_info.api_key, coinbase_info.api_secret)
+
+price = client.get_buy_price(currency_pair = 'BTC-USD')
+
+print(price)
