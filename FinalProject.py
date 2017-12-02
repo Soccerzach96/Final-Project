95d830039d1f05d43ca86a55db44f28a3f2f7e57
Zach <strongz@umich.edu>
2017-12-02 14:31:53 -0500

    GDAX Functionality

--- FinalProject.py
+++ FinalProject.py
@@ -36,10 +36,11 @@ for product in client.get_products():
 # currencies = client.get_products()
 # print(currencies)
 
-
+# Returns the Price of Bitcoin every 30 days (Reverse from today's date)
 
 # Returns the Price of Ethereum every 30 days (Reverse from today's date)
 ether = client.get_product_historic_rates('ETH-USD', granularity = 60*60*24*30, start = '2015-01-05T00:00:00+00:00')
-# ether = client.getProductHistoricRates('BTC-USD', {'start': "2015-01-05T00:00:00+00:00"}, {'granularity': 60*60*24*30})
 for e in ether:
 	print(e[3])
+
+# Returns the Price of Litecoin every 30 days (Reverse from today's date)
